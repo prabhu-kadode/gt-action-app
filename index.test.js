c7bf0f4b3197e07f9a1abc015329b7813fc2557d
prabhu <prabhukadode1@gmail.com>
2026-06-25 10:36:43 +0530

    substraction

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const add = require('./index')
+const {add,sub} = require('./index')
 
 test("add 1+2",()=>{
     expect(add(1,2)).toBe(3)
@@ -10,4 +10,9 @@ test("add 10, 2",()=>{
 
 test("add 10, 20 should return 30",()=>{
     expect(add(10,30)).toBe(40)
+})
+
+
+test("sub 20,10 should return 10",()=>{
+    expect(sub(20,10)).toBe(10)
 })
