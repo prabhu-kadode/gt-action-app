4ea72c5884aaeb3bdf43c68b80118a4a2dd58c2d
prabhu <prabhukadode1@gmail.com>
2026-06-25 19:21:47 +0530

    testing coverage

--- index.test.js
+++ index.test.js
@@ -4,15 +4,15 @@ test("add 1+2",()=>{
     expect(add(1,2)).toBe(3)
 })
 
-test("add 10, 2",()=>{
-    expect(add(10,2)).toBe(12)
-})
+// test("add 10, 2",()=>{
+//     expect(add(10,2)).toBe(12)
+// })
 
-test("add 10, 20 should return 30",()=>{
-    expect(add(10,30)).toBe(40)
-})
+// test("add 10, 20 should return 30",()=>{
+//     expect(add(10,30)).toBe(40)
+// })
 
 
-test("sub 20,10 should return 10",()=>{
-    expect(sub(20,10)).toBe(10)
-})
+// test("sub 20,10 should return 10",()=>{
+//     expect(sub(20,10)).toBe(10)
+// })
